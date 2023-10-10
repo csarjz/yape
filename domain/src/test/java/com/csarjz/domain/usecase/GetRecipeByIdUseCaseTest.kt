2b7da7bf68c784ec26c2beb299e56ea66d38bbf6
César Zorrilla <csarjz@gmail.com>
2023-10-10 13:09:35 -0500

    feat: add GetRecipeByIdUseCase and update the repository

---
+++ domain/src/test/java/com/csarjz/domain/usecase/GetRecipeByIdUseCaseTest.kt
@@ -0,0 +1,39 @@
+package com.csarjz.domain.usecase
+
+import com.csarjz.domain.model.Recipe
+import com.csarjz.domain.repository.RecipeRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class GetRecipeByIdUseCaseTest {
+
+    @RelaxedMockK
+    private lateinit var recipeRepository: RecipeRepository
+
+    private val getRecipeByIdUseCase by lazy { GetRecipeByIdUseCase(recipeRepository) }
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this)
+
+    @Test
+    fun `when useCase is invoked should return recipe from repository`() = runTest {
+        val recipeId = "123"
+        val recipe: Recipe = mockk()
+
+        coEvery { recipeRepository.getRecipeById(any()) } returns recipe
+        val result = getRecipeByIdUseCase(recipeId)
+
+        Assert.assertEquals(result, recipe)
+        coVerify(exactly = 1) { recipeRepository.getRecipeById(recipeId) }
+        coVerify(exactly = 0) { recipeRepository.getRecipes() }
+        coVerify(exactly = 0) { recipeRepository.refreshFromRemoteRecipes() }
+        coVerify(exactly = 0) { recipeRepository.saveRecipesLocally(any()) }
+    }
+}
